a9f5ee6455af781d3edc7b985b1816aa766ce753
Faysal-A-S <faysala1655@gmail.com>
2025-05-25 17:18:17 +0600

    feat: title change

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,7 +10,7 @@ function RouteComponent() {
   return (
     <>
       <ThemeToggle />
-      <div className="bg-blue-500 dark:bg-gray-500 md:pb-20 pb-4 pt-6 md:pt-20 ">
+      <div className="bg-blue-500 dark:bg-gray-500 md:pb-16 pb-4 pt-6 md:pt-16 ">
         <div className=" max-w-lg  mx-4 md:mx-auto bg-white dark:bg-gray-900 shadow-md rounded-lg overflow-hidden transition-colors duration-300">
           <div className="w-full max-w-lg bg-white dark:bg-gray-900 shadow-lg rounded-lg overflow-hidden transition-colors duration-300">
             <ChatComponent />
